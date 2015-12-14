495fb8d0035d31f23de3ef9e6936980aa6c1dbdf
Cem Turan <cem2ran@gmail.com>
2015-12-14 10:42:42 +0100

    hot reloading magic!

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,9 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
   context: __dirname,
   cache: true,
-  devtool: 'eval-source-map',
+  devtool: 'cheap-module-eval-source-map',
   entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
+    'webpack-hot-middleware/client',
     './app/App'
   ],
   output: {
